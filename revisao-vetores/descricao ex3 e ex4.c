c7618c433773af8436dc6e13343af70b7340274a
Gilberto V Oliveira <45500064+gvoliveira@users.noreply.github.com>
2023-06-25 18:20:58 -0300

    Update descricao ex3 e ex4.c

--- revisao-vetores/descricao ex3 e ex4.c
+++ revisao-vetores/descricao ex3 e ex4.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-/*Faça um programa que leia uma matriz 3x3 do tipo inteiro.
-Após a leitura da matriz:
+/*FaÃ§a um programa que leia uma matriz 3x3 do tipo inteiro.
+ApÃ³s a leitura da matriz:
 a)indique a linha e a coluna onde esta o menor elemento.
 b)imprima os elementos da diagonal principal da matriz.
 c)Imprima toda a matriz
 */
-
-/*Faça um programa que leia um vetor de tamanho 6 de variaveis inteiras
-Após a leitura desse vetor, você deve ordena-lo. Por fim
-imprimi-lo*/
-
 
 int main(){
     int mat[3][3];
