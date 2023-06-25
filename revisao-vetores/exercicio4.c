3056ca2298f710b36e15e9fc39213a56c8bd8048
Gilberto V Oliveira <45500064+gvoliveira@users.noreply.github.com>
2023-06-25 18:22:50 -0300

    ex4

---
+++ revisao-vetores/exercicio4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Faça um programa que leia um vetor de tamanho 6 de variaveis inteiras
+Após a leitura desse vetor, você deve ordena-lo. Por fim
+imprimi-lo*/
+int main(){
+    int vetor[6];
+
+
+
+    return 0;
+}
